a6a0110095796539750acbc74eb2a52775b6345d
iacy19 <105487962+iacy19@users.noreply.github.com>
2022-06-29 23:54:31 +0530

    Add files via upload

---
+++ Array/First and last occurrences of x.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+vector<int> find(int arr[], int n , int x )
+{
+    // code here
+    
+    vector<int>v;
+    
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i]==x)
+        {
+            v.push_back(i);
+            break;
+        }
+    }
+    
+    
+    for(int j=n-1; j>=0; j--)
+    {
+        if(arr[j]==x)
+        {
+            v.push_back(j);
+            break;
+        }
+    }
+    
+    
+    if(v.size()==0)
+    {
+        v.push_back(-1);
+        v.push_back(-1);
+    }
+    
+    return v;
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int arr[n],i;
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        vector<int> ans;
+        ans=find(arr,n,x);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+
+
+  // } Driver Code Ends
